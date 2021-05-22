f84e25be28b87d987d63942c61d203bbcfecfb02
TookTalk2Mac <wootaejung24@gmail.com>
2021-05-22 19:03:23 +0900

    fix try catch in signup.js

--- signup.js
+++ signup.js
@@ -5,14 +5,20 @@ async function handleSubmit(event) {
   const data = new FormData(event.target);
   const value = Object.fromEntries(data.entries());
 
-  let res = await fetch(`http://localhost:5000/sign-up`,{
-    method: 'post',
-    body : JSON.stringify(value),
-    headers:{
-      'Content-Type': 'application/json'
-    }
-  })
-  console.log(res.json())
+  try {
+    const res = await fetch(`http://localhost:5000/sign-up`,{
+      method: 'post',
+      body : JSON.stringify(value),
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    })
+    console.log(res.status)
+    
   }
+  catch(e){
+    console.log(e)
+  }
+}
 
 form.addEventListener('submit', handleSubmit);
